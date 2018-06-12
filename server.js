26525f31d46e11ccc653c52d79c4e750ef1e69f6
ulises zaragoza <Ulises@10-248-167-16.wireless.oregonstate.edu>
2018-06-12 11:03:26 -0700

    new server.js uploaded

--- server.js
+++ server.js
@@ -1,50 +1,190 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+var bodyParser = require('body-parser');
 
-var http = require('http');
-var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
 
-console.log("== Reading in index.html ==");
-var dreamIndex = fs.readFileSync('public/index.html', 'utf8');
+var mongoHost = process.env.MONGO_HOST || "classmongo.engr.oregonstate.edu";
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME || "cs290_zaragozu";
+var mongoPassword = process.env.MONGO_PASSWORD || "cs290_zaragozu";
+var mongoDBName = process.env.MONGO_DB_NAME || "cs290_zaragozu";
+var mongoURL = "mongodb://" +
+  mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort +
+  "/" + mongoDBName;
 
-console.log("== Reading in index.js ==");
-var dreamJS = fs.readFileSync('public/index.js', 'utf8');
-
-console.log("== Reading in style.css ==");
-var dreamCSS = fs.readFileSync('public/style.css', 'utf8');
-
-console.log("== Reading in 404.html ==");
-var dreamError = fs.readFileSync('public/404.html', 'utf8');
+  var mongoDB = null;
 
 
-function requestHandler (request, response){
-  console.log("== Got a request");
-  console.log("  -- method:", request.method);
-  console.log("  -- url:", request.url);
-  if (request.url === '/index.html' || request.url === '/'){
-    response.statusCode = 200;
-    console.log("status code:", response.statusCode);
-    response.write(dreamIndex);
-  }
-  else if (request.url === '/index.js'){
-    response.statusCode = 200;
-    console.log("status code:", response.statusCode);
-    response.write(dreamJS);
-  }
-  else if (request.url === '/style.css'){
-    response.statusCode = 200;
-    console.log("status code:", response.statusCode);
-    response.write(dreamCSS);
-  }
-  else{
-    response.statusCode = 404;
-    console.log("status code:", response.statusCode);
-    response.write(dreamError);
-  }
-  response.end();
-}
+var port = 3000 || process.env.PORT;
 
-var server = http.createServer(requestHandler);
-var port = process.env.PORT || 3000;
 
-server.listen(port, function () {
-  console.log("== Server is listening on port " + port);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+/*
+app.get('/', function (req, res, next){
+  res.status(200).render('publicPage', {dream: dreamData});
+}); */
+
+
+app.get('/', function (req, res, next){
+
+  var dreamsCollection = mongoDB.collection('dreams');
+  dreamsCollection.find().toArray(function(err, dreamsDocs) {
+    if(err)
+    {
+      res.status(500).send("Error fetching dream from DB.");
+    }
+    else if(dreamsDocs.length > 0)
+    {
+      res.status(200).render('publicPage', {dream: dreamsDocs});
+    }
+    else{
+      next();
+    }
+  })
+
 });
+
+
+app.get('/personalPage', function (req, res, next){
+
+    var dreamsCollection = mongoDB.collection('dreams');
+    dreamsCollection.find().toArray(function(err, dreamsDocs) {
+      if(err)
+      {
+        res.status(500).send("Error fetching dream from DB.");
+      }
+      else if(dreamsDocs.length > 0)
+      {
+        res.status(200).render('personalPage', {dream: dreamsDocs});
+      }
+      else{
+        next();
+      }
+    })
+
+  });
+
+/*
+var person = req.params.person.toLowerCase();
+if (req.body && req.body.caption && req.body.photoURL) {
+  var photo = {
+    caption: req.body.caption,
+    photoURL: req.body.photoURL
+  };
+  var peopleCollection = mongoDB.collection('people');
+  peopleCollection.updateOne(
+    { personId: person },
+    { $push: { photos: photo } },
+    function (err, result) {
+      if (err) {
+        res.status(500).send("Error inserting photo into DB.")
+      } else {
+        console.log("== mongo insert result:", result);
+        if (result.matchedCount > 0) {
+          res.status(200).end();
+        } else {
+          next();
+        }
+      }
+    }
+  );
+} else {
+  res.status(400).send("Request needs a JSON body with caption and photoURL.")
+}*/
+
+
+app.post('/addDream', function (req, res, next) {
+  if (req.body && req.body.dreamer && req.body.dream_text && req.body.dream_title) {
+    var dream = {
+      dreamer: req.body.dreamer,
+      dream_text: req.body.dream_text,
+      dream_title: req.body.dream_title,
+      public: req.body.public
+    };
+    var dreamsCollection = mongoDB.collection('dreams');
+
+    dreamsCollection.insertOne(dream, function (err, db) {
+          if (err)
+          {
+            res.status(500).send("Error inserting photo into DB.")
+          }
+          else
+          {
+            res.status(200).end();
+          }
+    });
+
+  //  res.status(200).send("Dream successfully added");
+
+  } else {
+    res.status(400).send("Requests to this path must " +
+      "contain a JSON body with dreamer, dream-title, & dream-text " +
+      "fields.");
+  }
+});
+
+/*
+app.post('/addDream', function (req, res, next) {
+  if (req.body && req.body.dreamer && req.body.dream_text && req.body.dream_title) {
+    console.log("== Client added the following dream:");
+    console.log("  - person:", req.params.dreamer);
+    console.log("  - url:", req.body.url);
+    console.log("  - caption:", req.body.dream_text);
+
+    var dream = {
+      dreamer: req.body.dreamer,
+      dream_text: req.body.dream_text,
+      dream_title: req.body.dream_title,
+      public: req.body.public
+    };
+    dreamData.push(dream);
+    res.status(200).end();
+
+  //  res.status(200).send("Dream successfully added");
+
+  } else {
+    res.status(400).send("Requests to this path must " +
+      "contain a JSON body with dreamer, dream-title, & dream-text " +
+      "fields.");
+  }
+});*/
+
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+});
+
+
+MongoClient.connect(mongoURL, function (err, client) {
+  if (err) {
+    throw err;
+  }
+  mongoDB = client.db(mongoDBName);
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
